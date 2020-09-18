5fcc63df7496a98c8d396fa27dd7a767aad84393
Ahadian Akbar <rabkawork@gmail.com>
2020-09-18 11:00:46 +0700

    Update MainActivity:

--- app/src/main/java/com/rab/kotlin_sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/rab/kotlin_sharedpreferences/MainActivity.kt
@@ -2,10 +2,20 @@ package com.rab.kotlin_sharedpreferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_dashboard.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.btnLogin
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener {
+
+            Log.e("error","ee")
+        }
     }
 }
