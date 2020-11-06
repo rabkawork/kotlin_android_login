65658a0c0cb5023b6974bc667c4165c4467d5874
Ahadian Akbar <rabkawork@gmail.com>
2020-11-06 16:58:12 +0700

    [Update] MainActivity -> login function, validation, store the session, DashboardActivity -> logout function -> OnClick, showing username who the signed in into the apps

--- app/src/main/java/com/rab/kotlin_sharedpreferences/DashboardActivity.kt
+++ app/src/main/java/com/rab/kotlin_sharedpreferences/DashboardActivity.kt
@@ -1,11 +1,31 @@
 package com.rab.kotlin_sharedpreferences
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.rab.kotlin_sharedpreferences.Helper.SharedPreferences
+import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
+
+    lateinit var username: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_dashboard)
+        val session:SharedPreferences = SharedPreferences(this)
+
+        sess_username.setText("Welcome, " + session.getString("username"))
+
+        btnLogout.setOnClickListener {
+            session.clear()
+            gotoHomePage()
+        }
+    }
+
+
+    fun gotoHomePage(){
+        startActivity(Intent(this, MainActivity::class.java))
     }
 }
--- app/src/main/java/com/rab/kotlin_sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/rab/kotlin_sharedpreferences/MainActivity.kt
@@ -1,20 +1,54 @@
 package com.rab.kotlin_sharedpreferences
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.rab.kotlin_sharedpreferences.Helper.SharedPreferences
 import kotlinx.android.synthetic.main.activity_dashboard.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.btnLogin
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var username:EditText
+    lateinit var password:EditText
+    lateinit var btnSignIn:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        username = findViewById(R.id.etUsername)
+        password = findViewById(R.id.etPassword)
+        btnSignIn  = findViewById(R.id.btnLogin)
+        val session:SharedPreferences=SharedPreferences(this)
 
-        btnLogin.setOnClickListener {
-            Log.e("error","ee")
+        // check existing session
+        if(session.isLoggedIn("username") == true) {
+            gotoDashboard()
         }
+
+        btnSignIn.setOnClickListener {
+
+            val userValue = username.editableText.toString()
+            val passValue = password.editableText.toString()
+
+            // simple validation
+            if (userValue.isEmpty() or passValue.isEmpty()) {
+                Toast.makeText(this@MainActivity,"Please fill username and password form first",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                session.save("username",userValue)
+                gotoDashboard()
+            }
+        }
+    }
+
+    fun gotoDashboard(){
+        startActivity(Intent(this, DashboardActivity::class.java))
     }
 }
