f50786b7d2468dbb325fc2c85a50292d37758c5c
Ahadian Akbar <rabkawork@gmail.com>
2020-11-06 16:08:15 +0700

    [update] shared preferences with some functions

--- app/src/main/java/com/rab/kotlin_sharedpreferences/Helper/SharedPreferences.kt
+++ app/src/main/java/com/rab/kotlin_sharedpreferences/Helper/SharedPreferences.kt
@@ -10,4 +10,46 @@ import android.content.SharedPreferences
 class SharedPreferences(val context: Context) {
     private val PREFS_NAME = "rab_session_preferences"
     val sharedPref: SharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+
+    fun save(KEY_NAME: String, text: String) {
+        val editor: SharedPreferences.Editor = sharedPref.edit()
+        editor.putString(KEY_NAME, text)
+        editor!!.commit()
+    }
+
+    fun save(KEY_NAME: String, value: Int) {
+        val editor: SharedPreferences.Editor = sharedPref.edit()
+        editor.putInt(KEY_NAME, value)
+        editor.commit()
+    }
+
+    fun save(KEY_NAME: String, status: Boolean) {
+        val editor: SharedPreferences.Editor = sharedPref.edit()
+        editor.putBoolean(KEY_NAME, status!!)
+        editor.commit()
+    }
+
+    fun getString(KEY_NAME: String): String? {
+        return sharedPref.getString(KEY_NAME, null)
+    }
+
+    fun getInt(KEY_NAME: String): Int {
+        return sharedPref.getInt(KEY_NAME, 0)
+    }
+
+    fun getBoolean(KEY_NAME: String, defaultValue: Boolean): Boolean {
+        return sharedPref.getBoolean(KEY_NAME, defaultValue)
+    }
+
+    fun clear() {
+        val editor: SharedPreferences.Editor = sharedPref.edit()
+        editor.clear()
+        editor.commit()
+    }
+
+    fun remove(KEY_NAME: String) {
+        val editor: SharedPreferences.Editor = sharedPref.edit()
+        editor.remove(KEY_NAME)
+        editor.commit()
+    }
 }
--- app/src/main/java/com/rab/kotlin_sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/rab/kotlin_sharedpreferences/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnLogin.setOnClickListener {
-
             Log.e("error","ee")
         }
     }
